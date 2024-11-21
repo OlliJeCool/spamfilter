36116aadad27944de216061dc20cf7a578d88e83
OlliJeCool <ollihomail@gmail.com>
2024-11-21 18:53:35 +0100

    all functionallity for xx_sp_eval working and tested

--- src/corpus.py
+++ src/corpus.py
@@ -4,7 +4,7 @@ from src.exceptions.FolderNotFoundException import FolderNotFoundException
 
 class Corpus:
     def __init__(self, filepath: str):
-        self.filepath = filepath if self._path_exists(filepath) else None
+        self.filepath = filepath if Corpus.path_exists(filepath) else None
 
     def emails(self):
         if not self.filepath:
@@ -13,5 +13,6 @@ class Corpus:
             with open(os.path.join(self.filepath, file), "rt", encoding="utf-8") as f:
                 yield file,f.read()
 
-    def _path_exists(self, filepath: str):
+    @staticmethod
+    def path_exists(filepath: str):
         return os.path.isdir(filepath)
--- src/quality.py
+++ src/quality.py
@@ -1,4 +1,20 @@
+import os
+from .corpus import Corpus
+from .exceptions.FolderNotFoundException import FolderNotFoundException
+from .utils import read_classification_from_file
+from .confmat import BinaryConfusionMatrix
+
 def quality_score(tp:int, tn:int, fp:int, fn: int) -> int:
     true_count = tp+tn
     denom = true_count + (10*fp)+fn
     return true_count/denom
+
+def compute_quality_for_corpus(corpus_dir:str):
+    if not Corpus.path_exists(corpus_dir):
+        raise FolderNotFoundException("Invalid corpus directory.")
+    
+    truth_dict = read_classification_from_file(os.path.join(corpus_dir, "!truth.txt"))
+    pred_dict = read_classification_from_file(os.path.join(corpus_dir, "!prediction.txt"))
+    confmat = BinaryConfusionMatrix("SPAM", "OK")
+    confmat.compute_from_dicts(truth_dict, pred_dict)
+    return quality_score(confmat.tp, confmat.tn, confmat.fp, confmat.fn) 
